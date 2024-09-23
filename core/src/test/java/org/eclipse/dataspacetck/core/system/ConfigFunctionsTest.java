856148efc65992fb16867d1aa8ec170d2c055228
Jim Marino <jim.marino@gmail.com>
2024-09-23 16:26:17 +0200

    Add license header

--- core/src/test/java/org/eclipse/dataspacetck/core/system/ConfigFunctionsTest.java
+++ core/src/test/java/org/eclipse/dataspacetck/core/system/ConfigFunctionsTest.java
@@ -6,6 +6,20 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/*
+ *  Copyright (c) 2024 Metaform Systems, Inc.
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Metaform Systems, Inc. - initial API and implementation
+ *
+ */
+
 class ConfigFunctionsTest {
     private static final String PROP_1 = "PROP1";
     private static final String PROP_1_VALUE = "PROP_1_VALUE";
